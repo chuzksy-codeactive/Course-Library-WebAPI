adc9ea846e771c06932c254a0b6e9cadae7379e2
onuchukwuchika <chuzksy@gmail.com>
2019-11-12 07:39:29 +0100

    Supporting OPTION http method

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -59,5 +59,12 @@ namespace Library.API.Controllers
             var authorToReturn = _mapper.Map<AuthorDto> (authorEntity);
             return CreatedAtRoute ("GetAuthor", new { authorId = authorToReturn.Id }, authorToReturn);
         }
+
+        [HttpOptions]
+        public IActionResult GetAuthorsOptions ()
+        {
+            Response.Headers.Add ("Allow", "GET,OPTIONS,POST");
+            return Ok ();
+        }
     }
 }
