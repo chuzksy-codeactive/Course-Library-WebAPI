47f6a94a1378b23c60300adb67456367f295bb43
onuchukwuchika <chuzksy@gmail.com>
2019-11-15 02:04:39 +0100

    Include orderBy clause to the pagination links

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -114,6 +114,7 @@ namespace Library.API.Controllers
                     return Url.Link ("GetAuthors",
                         new
                         {
+                            orderBy = authorsResourceParameters.OrderBy,
                             pageNumber = authorsResourceParameters.PageNumber - 1,
                             pageSize = authorsResourceParameters.PageSize,
                             mainCategory = authorsResourceParameters.MainCategory,
@@ -123,6 +124,7 @@ namespace Library.API.Controllers
                     return Url.Link ("GetAuthors",
                         new
                         {
+                            orderBy = authorsResourceParameters.OrderBy,
                             pageNumber = authorsResourceParameters.PageNumber + 1,
                             pageSize = authorsResourceParameters.PageSize,
                             mainCategory = authorsResourceParameters.MainCategory,
@@ -132,6 +134,7 @@ namespace Library.API.Controllers
                     return Url.Link ("GetAuthors",
                         new
                         {
+                            orderBy = authorsResourceParameters.OrderBy,
                             pageNumber = authorsResourceParameters.PageNumber,
                             pageSize = authorsResourceParameters.PageSize,
                             mainCategory = authorsResourceParameters.MainCategory,
